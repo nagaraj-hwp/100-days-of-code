ae88b95b7b189d48055e4b68291e20f4b5b4f3a9
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-01-29 21:31:42 +0530

    Day 1: print, input, variables completed

---
+++ Day 1/variables.py
@@ -0,0 +1,57 @@
+# print('Hello World!')
+# # understanding strings
+# # print("Hello world!")
+# print("\n")
+# print('This is a day 1 exercise')
+
+# print('print statement should be like below example')
+# print("print('Write something here to print')")
+# print("print(\"Write something here to print\")")
+# print('print(\'Write something here to print\')')
+# print('print("Write something here to print")')
+# print("\n")
+# # printing multiple strings in multiple lines usNaing single print statement
+
+# print("Hello world!\nHello World!\nHello world!")
+# print("\n")
+
+# # concat in same print statement
+# print("Hello" + " " + "Nagaraj")
+# print("Hello Nagaraj")
+# print("\n")
+
+# # Keeping space or indent is very much important in python
+# print("Keeping space or indent is very much important in python")
+# print("\n")
+
+# ####################################################################
+# #understanding input method 
+# input("What is your name: ")
+# print("above is a example for a input prompt")
+
+# print("Hello " + input("what is your name: ")+ ", How are you?")
+
+# print("\n")
+
+# name = input("what is your name: ")
+# print(name)
+
+# length = len(name)
+
+# print(length)
+
+
+#1. Create a greeting for your program.
+print("HI Coder, Welcome to the band name generator.")
+#2. Ask the user for the city that they grew up in.
+city_name = input("Enter the name of the city where you born:\n")
+#3. Ask the user for the name of a pet.
+pet_name = input("Enter the name of your pet:\n")
+#4. Combine the name of their city and pet and show them their band name.
+print()
+band_name = city_name + " " + pet_name
+print(band_name + "\n")
+#5. Make sure the input cursor shows on a new line:
+
+
+# Solution: https://replit.com/@appbrewery/band-name-generator-end
