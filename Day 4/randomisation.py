5e0bfc74d023481b5e2eca181f317f3a863f2a47
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-02-02 14:21:23 +0530

    Day 4: random float generation

--- Day 4/randomisation.py
+++ Day 4/randomisation.py
@@ -1,8 +1,19 @@
 # understanding randomisation
 import random
 import my_module
-# random_integer = random.randint(1, 100)
-# print(random_integer)
+random_integer = random.randint(1, 100)
+print(random_integer)
+
+# random float with random.random() produces between 0.00000000 to 0.99999999999999...
+random_float = random.random()
+print(random_float)
+
+# random float with random.random() can be multiplied with 5 or 10 to produce within its range.
+random_float = random.random() * 5
+print(random_float)
+
+
+
 
 # Accessing the other modules
 # print(my_module.a)
