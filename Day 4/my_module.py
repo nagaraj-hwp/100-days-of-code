4b8afe7c8373c1337068fbe9ca23cb2b242f2ae8
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-02-02 14:10:59 +0530

    Day 4: module understanding

---
+++ Day 4/my_module.py
@@ -0,0 +1,11 @@
+# Understnading modules in a python 
+
+a = []
+b = [1, 2, 3]
+c = ['a', 'b', 'c']
+d = ["apple", "ball"]
+e = [1, 'select', True]
+
+pi = 3.14
+
+# list can be any datatype collection
