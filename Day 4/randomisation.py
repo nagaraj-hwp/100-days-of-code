a8f476b1fcba978a2f65e69756e7b28bdc56aba2
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-02-02 14:13:15 +0530

    Day 4: randomisation with random module

--- Day 4/randomisation.py
+++ Day 4/randomisation.py
@@ -1,2 +1,12 @@
 # understanding randomisation
 import random
+import my_module
+# random_integer = random.randint(1, 100)
+# print(random_integer)
+
+# Accessing the other modules
+# print(my_module.a)
+# print(my_module.b)
+# print(my_module.c)
+# print(my_module.d)
+# print(my_module.e)
