4358768f0a41b19ef9b22c78a761a5cc5d421536
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-02-02 14:47:36 +0530

    Day 4: Simple head or tails

--- Day 4/randomisation.py
+++ Day 4/randomisation.py
@@ -21,3 +21,12 @@ print(random_float)
 # print(my_module.c)
 # print(my_module.d)
 # print(my_module.e)
+
+# simple head or tails
+
+head_or_tail = random.randint(0, 1)
+if head_or_tail == 1:
+    print("Heads")
+else:
+    print("Tails")
+
