8d9f6cb06feb427763ddf0b7101c9af3c5926d0a
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-02-03 21:07:49 +0530

    Day 5: Even sum exercise

--- day_5/for_loops.py
+++ day_5/for_loops.py
@@ -18,17 +18,17 @@ for fruit in fruits:
 # 1146 ÷ 7 = 163.71428571428572
 # Average height rounded to the nearest whole number = 164
 # Input a Python list of student heights
-student_heights = input().split()
-for n in range(0, len(student_heights)):
-    student_heights[n] = int(student_heights[n])
+# student_heights = input().split()
+# for n in range(0, len(student_heights)):
+#     student_heights[n] = int(student_heights[n])
 
-tot = 0
-for i in student_heights:
-    tot += i
-student = len(student_heights)
-print("total height = ", tot)
-print("number of students = ", student)
-print("average height = ", round(tot/student))
+# tot = 0
+# for i in student_heights:
+#     tot += i
+# student = len(student_heights)
+# print("total height = ", tot)
+# print("number of students = ", student)
+# print("average height = ", round(tot/student))
 
 
 # write a program that calculates the highest score from a List of scores.
@@ -37,12 +37,29 @@ print("average height = ", round(tot/student))
 # The highest score in the class is: x
 
 # Input a list of student scores
-student_scores = input().split()
-for n in range(0, len(student_scores)):
-    student_scores[n] = int(student_scores[n])
+# student_scores = input().split()
+# for n in range(0, len(student_scores)):
+#     student_scores[n] = int(student_scores[n])
 
-max_grade = 0
-for grade in student_scores:
-    if max_grade < grade:
-        max_grade = grade
-print("The highest score in the class is:", max_grade)
+# max_grade = 0
+# for grade in student_scores:
+#     if max_grade < grade:
+#         max_grade = grade
+# print("The highest score in the class is:", max_grade)
+
+# write a program that calculates the sum of all the even numbers from 1 to X. If X is 100 then the first even number would be 2 and the last one is 100:
+# i.e. 2 + 4 + 6 + 8 +10 ... + 98 + 100
+# Important, there should only be 1 print statement in your console output. It should just print the final total and not every step of the calculation.
+# Also, we will constrain the inputs to only take numbers from 0 to a max of 1000.
+
+# target = int(input())  # Enter a number between 0 and 1000
+# total = 0
+# for num in range(target+1):
+#     if num % 2 == 0:
+#         total += num
+# print(total)
+
+# total = 0
+# for num in range(2, target+1, 2):
+#     total += num
+# print(total)
