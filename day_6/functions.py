9f7fa357f6b967df40000511fa120e7e1b459bee
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-02-05 19:40:20 +0530

    Day 6: functions basics

--- day_6/functions.py
+++ day_6/functions.py
@@ -1,2 +1,11 @@
 # this is a file will hold functions realted learnings
 # Understanding functions
+# every built in methods are helps us achieve our solution
+
+# URL for built in python functions documentation
+# https://docs.python.org/3/library/functions.html
+
+def my_function():
+    print("Hello, This is inside my function.")
+
+my_function()
