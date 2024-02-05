80035f4bd45f85465b0bdc99d249c126e4155244
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-02-05 20:36:33 +0530

    Day 6: while loops basics

--- day_6/while_loop.py
+++ day_6/while_loop.py
@@ -2,3 +2,13 @@
 # while something_is_true:
 #     Do this
 
+# while True:
+#     print("This is a infinite loop")
+
+# while 1:
+#     print("This is a infinite loop")
+
+num = 5
+while num:
+    print(num)
+    num -= 1
