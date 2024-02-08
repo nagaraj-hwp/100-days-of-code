1d81a9dd8e133366539a02e62a7b0877443d5918
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-02-08 17:29:47 +0530

    Day 10: Function with outputs

--- day_10/functions_with_outputs.py
+++ day_10/functions_with_outputs.py
@@ -1,1 +1,9 @@
 # Understanding Function with outputs
+
+# def format_name(f_name, l_name):
+#     # f_name = "Nagaraj"
+#     # l_name = "Palpandi"
+#     return f"{f_name.title()} {l_name.title()}"
+    
+
+# print(format_name("nagaraj", "NaGaraJ"))
