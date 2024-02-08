a5ec66fa02f001942929d2c2a585d94be480d48b
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-02-08 14:20:43 +0530

    Day 8: Dictinary second challenge completed

--- day_9/dictionaries.py
+++ day_9/dictionaries.py
@@ -84,31 +84,32 @@
 # Write a function that will work with the following line of code to add the entry for Brazil to the travel_log.
 # add_new_country("Brazil", 5, ["Sao Paulo", "Rio de Janeiro"])
 
-country = input() # Add country name
-visits = int(input()) # Number of visits
-list_of_cities = eval(input()) # create list from formatted string
+# country = input() # Add country name
+# visits = int(input()) # Number of visits
+# list_of_cities = eval(input()) # create list from formatted string
 
-travel_log = [
-  {
-    "country": "France",
-    "visits": 12,
-    "cities": ["Paris", "Lille", "Dijon"]
-  },
-  {
-    "country": "Germany",
-    "visits": 5,
-    "cities": ["Berlin", "Hamburg", "Stuttgart"]
-  },
-]
+# travel_log = [
+#   {
+#     "country": "France",
+#     "visits": 12,
+#     "cities": ["Paris", "Lille", "Dijon"]
+#   },
+#   {
+#     "country": "Germany",
+#     "visits": 5,
+#     "cities": ["Berlin", "Hamburg", "Stuttgart"]
+#   },
+# ]
 
-def add_new_country(country, visits, list_of_cities):
-  new_dict = {}
-  new_dict["country"] = country
-  new_dict["visits"] = visits
-  new_dict["cities"] = list_of_cities
-  travel_log.append(new_dict)
+# def add_new_country(country, visits, list_of_cities):
+#   new_dict = {}
+#   new_dict["country"] = country
+#   new_dict["visits"] = visits
+#   new_dict["cities"] = list_of_cities
+#   travel_log.append(new_dict)
 
-add_new_country(country, visits, list_of_cities)
-print(f"I've been to {travel_log[2]['country']} {travel_log[2]['visits']} times.")
-print(f"My favourite city was {travel_log[2]['cities'][0]}.")
+# add_new_country(country, visits, list_of_cities)
+# print(f"I've been to {travel_log[2]['country']} {travel_log[2]['visits']} times.")
+# print(f"My favourite city was {travel_log[2]['cities'][0]}.")
 
+# -------------------------------------------------------------------
