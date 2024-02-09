c31fcca48706c78bdf4d1a19b7f12bb2bb4d52e4
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-02-09 17:59:37 +0530

    Day 12: Scope in python start

--- day_12/scope.py
+++ day_12/scope.py
@@ -1,9 +1,27 @@
 # Understanding Scopes in Python
-count = 1
+# count = 1
 
-def increase_count():
-    count = 2
-    print(f"Count inside function: {count}")
+# def increase_count():
+#     count = 2
+#     print(f"Count inside function: {count}")
 
-increase_count()
-print(f"Count Outside function: {count}")
+# increase_count()
+# print(f"Count Outside function: {count}")
+
+
+# Local Scope
+# def count_function():
+#     count = 2 # local to function
+#     print(f"Count inside function: {count}")
+
+# count_function()
+# print(f"Count Outside function: {count}")
+
+# Block Scope
+# in python unlike c, c++, Java there is nothing called block scope
+# eg:
+fill = "Red"
+if fill == "Red":
+    color = "Blue"
+
+print(color) # Variable insode if block works outside as well.
