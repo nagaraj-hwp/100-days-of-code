0f44557699a4c77f622724a7fe3a4a1bffc59c34
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-02-09 17:24:37 +0530

    Day 12: Scope in python start

--- day_12/scope.py
+++ day_12/scope.py
@@ -1,1 +1,9 @@
 # Understanding Scopes in Python
+count = 1
+
+def increase_count():
+    count = 2
+    print(f"Count inside function: {count}")
+
+increase_count()
+print(f"Count Outside function: {count}")
