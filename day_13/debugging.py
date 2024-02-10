574312b29c05ded79abafdb1a84cb97ce51c72bd
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-02-10 14:45:21 +0530

    Day 13: Debugging starts

---
+++ day_13/debugging.py
@@ -0,0 +1,13 @@
+# Learning to Debug the code in python
+
+############DEBUGGING#####################
+
+# Describe Problem
+def my_function():
+  for i in range(1, 20):
+  # for i in range(1, 20+1):
+    if i == 20:
+      print("You got it")
+my_function()
+
+
