1cfd6c833fa67ea72104e4ec9c7f42635a865f26
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-02-10 14:55:09 +0530

    Day 13: Debugging starts

---
+++ day_13/challenges.py
@@ -0,0 +1,38 @@
+# Add or Even debugging
+# LESSON 25 DAY 13 - DEBUGGING ODD or EVEN
+# number = int(input()) # Which number do you want to check?
+
+# if number % 2 == 0:
+#     print("This is an even number.")
+# else:
+#     print("This is an odd number.")
+
+
+# LESSON 26 DAY 13 - DEBUGGING LEAP YEAR
+
+# Which year do you want to check?
+# year = int(input())
+
+# if year % 4 == 0:
+#     if year % 100 == 0:
+#         if year % 400 == 0:
+#             print("Leap year.")
+#         else:
+#             print("Not leap year.")
+#     else:
+#         print("Leap year.")
+# else:
+#     print("Not leap year.")
+
+# LESSON 27 DAY 13 - DEBUGGING FIZZBUZZ
+
+# target = int(input())
+# for number in range(1, target + 1):
+#     if number % 3 == 0 and number % 5 == 0:
+#         print("FizzBuzz")
+#     elif number % 3 == 0:
+#         print("Fizz")
+#     elif number % 5 == 0:
+#         print("Buzz")
+#     else:
+#         print(number)
