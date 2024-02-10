aa1b57bb9eac13e8b8d848666ac4cd2da052339f
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-02-10 14:47:41 +0530

    Day 13: Debugging starts

--- day_13/debugging.py
+++ day_13/debugging.py
@@ -2,12 +2,20 @@
 
 ############DEBUGGING#####################
 
-# Describe Problem
-def my_function():
-  # for i in range(1, 20):
-  for i in range(1, 20+1):
-    if i == 20:
-      print("You got it")
-my_function()
+# # Describe Problem
+# def my_function():
+#   # for i in range(1, 20):
+#   for i in range(1, 20+1):
+#     if i == 20:
+#       print("You got it")
+# my_function()
+
+# Reproduce the Bug
+from random import randint
+dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
+dice_num = randint(1, 6)
+# dice_num = randint(0, 5)
+print(dice_num)
+print(dice_imgs[dice_num])
 
 
