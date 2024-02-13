7f7766d9f55fe954ca24ddf0c1756ee161753f23
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-02-13 21:38:00 +0530

    Day 16: Oops basics

---
+++ day_16/oops_basics.py
@@ -0,0 +1,25 @@
+# from turtle import Turtle, Screen
+# jack = Turtle()
+# print(jack)
+# jack.shape("classic")
+#
+# jack.color("red")
+# jack.forward(100)
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.add_column("City name", ["Adelaide", "Brisbane", "Darwin", "Hob  art", "Sydney", "Melbourne", "Perth"])
+table.add_column("Area", [1295, 5905, 112, 1357, 2058, 1566, 5386])
+table.add_column("Population", [1158259, 1857594, 120900, 205556, 4336374, 3806092, 1554769])
+table.add_column("Annual Rainfall", [600.5, 1146.4, 1714.7, 619.5, 1214.8, 646.9, 869.4])
+
+print(table)
+
+table.align = 'l'
+
+print(table)
