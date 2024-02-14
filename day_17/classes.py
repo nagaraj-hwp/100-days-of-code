8bcfadebf612eb459c6e3324ee08b970b3e99c92
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-02-14 20:10:56 +0530

    Day 17: Classes and objects starts

--- day_17/classes.py
+++ day_17/classes.py
@@ -5,10 +5,21 @@ class User:
         print("Constructor been called")
         self.id = id
         self.username = username
+        self.followers = 0
+        self.following = 0
+    
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
 
 user_1 = User("001", "nagaraj_hwp")
-print(user_1)
 user_2 = User("002", "common_man_says")
-print(user_2)
+
+user_1.follow(user_2)
 
 
+print(user_1.following)
+print(user_2.following)
+print(user_1.followers)
+print(user_2.followers)
