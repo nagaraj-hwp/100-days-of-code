faac66fd611adf015cc0865f3962664897f027f2
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-02-14 21:29:53 +0530

    Day 17: Quiz game completed

--- day_17/quiz_game/main.py
+++ day_17/quiz_game/main.py
@@ -1,12 +1,23 @@
 from question_model import Question
-from data import question_data
+from data import question_data, another_question_data
+from quiz_brain import QuizBrain
 
 question_bank = []
 
-for question in question_data:
-    question_text = question["text"]
-    question_answer = question["answer"]
+for question in another_question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
-print(question_bank)
+quiz = QuizBrain(question_bank)
+
+
+while quiz.still_have_questions():
+    quiz.next_question()
+
+final_score = quiz.get_final_score()
+print("--------------------------------")
+print("| You have completed the Quiz! |")
+print(f"| Your Final score is {final_score}/{len(question_bank)}     |")
+print("--------------------------------")
--- day_17/quiz_game/question_model.py
+++ day_17/quiz_game/question_model.py
@@ -4,3 +4,5 @@ class Question():
         self.answer = answer
 
 
+
+
