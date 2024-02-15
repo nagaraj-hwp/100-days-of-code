dfd6be69aa179c4570cfa3873ba1ce2e1ab1859f
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-02-15 23:08:34 +0530

    Day 18: Turtle random walk completed with random RGB colours

--- day_18/random_walk.py
+++ day_18/random_walk.py
@@ -1,21 +1,31 @@
 # Random walk program using python turtle
-
-from turtle import Turtle, Screen
+import turtle as t
 import random
 
-tim = Turtle()
-
+tim = t.Turtle()
+t.colormode(255)
 colours = ["red", "green", "blue", "orange", "purple", "pink", "yellow", "cyan", "magenta", "brown", "violet", "azure",
            "bisque1", "burlywood1"]
 
 directions = [0, 90, 180, 270]
 
-for _ in range(100):
+
+def get_colour():
+    red = random.randint(0, 255)
+    green = random.randint(0, 255)
+    blue = random.randint(0, 255)
+    rgb = (red, green, blue)
+    return rgb
+
+
+for _ in range(200):
     tim.speed("fastest")
-    tim.color(random.choice(colours))
+    # tim.color(random.choice(colours))
+    tim.pencolor(get_colour())
     tim.pensize(10)
     tim.forward(30)
     tim.setheading(random.choice(directions))
 
-screen = Screen()
+screen = t.Screen()
 screen.exitonclick()
+
