bee18249aa4deafecf0ff6221455cf1a5b681de3
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-02-15 20:26:08 +0530

    Day 18: Turtle module simple pendown penup

--- day_18/turtle_deepdive.py
+++ day_18/turtle_deepdive.py
@@ -1,10 +1,34 @@
+# from turtle import Turtle, Screen
+#
+# tim = Turtle()
+# tim.shape("turtle")
+# tim.color("red")
+# tim.forward(100)
+# tim.right(170)
+# tim.forward(100)
+# tim.right(90)
+# tim.forward(100)
+# tim.right(90)
+# tim.forward(100)
+# tim.right(90)
+# tim.forward(100)
+#
+# screen = Screen()
+# screen.exitonclick()
+
+
 from turtle import Turtle, Screen
 
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
-
+tim = Turtle()
+tim.shape("turtle")
+tim.color("red")
+for _ in range(5):
+    tim.forward(10)
+    tim.penup()
+    tim.forward(10)
+    tim.pendown()
 
 
 screen = Screen()
-
 screen.exitonclick()
+
