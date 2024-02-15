84e67b63a2610101dfce1145090c6920fcccadb5
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-02-15 22:57:20 +0530

    Day 18: Turtle random walk completed with pre defined colurs

--- day_18/random_walk.py
+++ day_18/random_walk.py
@@ -1,2 +1,21 @@
 # Random walk program using python turtle
 
+from turtle import Turtle, Screen
+import random
+
+tim = Turtle()
+
+colours = ["red", "green", "blue", "orange", "purple", "pink", "yellow", "cyan", "magenta", "brown", "violet", "azure",
+           "bisque1", "burlywood1"]
+
+directions = [0, 90, 180, 270]
+
+for _ in range(100):
+    tim.speed("fastest")
+    tim.color(random.choice(colours))
+    tim.pensize(10)
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
+
+screen = Screen()
+screen.exitonclick()
