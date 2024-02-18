b4f0afd2bf74ba3746b0a24dcb73ce6b77bc3686
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-02-18 16:12:58 +0530

    Day 21: day 20 Snake game controls completed

--- day_20/main.py
+++ day_20/main.py
@@ -1,5 +1,6 @@
 # Snake game project
-from turtle import Turtle, Screen
+from turtle import Screen
+from snake import Snake
 import time
 
 screen = Screen()
@@ -8,25 +9,13 @@ screen.setup(width=600, height=600)
 screen.title("Epic Snake game")
 screen.tracer(0)
 
-initial_turtles = [(0, 0), (-20, 0), (-40, 0)]
-segments = []
-for position in initial_turtles:
-    new_segment = Turtle("square")
-    new_segment.color("white")
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
+snake = Snake()
 
-# segment_1 = Turtle("Square")
-# segment_1.color("white")
-#
-# segment_2 = Turtle("Square")
-# segment_2.color("white")
-# segment_2.goto(-20, 0)
-#
-# segment_3 = Turtle("Square")
-# segment_3.color("white")
-# segment_3.goto(-40, 0)
+screen.listen()
+screen.onkey(key="Up", fun=snake.up)
+screen.onkey(key="Down", fun=snake.down)
+screen.onkey(key="Left", fun=snake.left)
+screen.onkey(key="Right", fun=snake.right)
 
 game_is_on = True
 
@@ -34,11 +23,7 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
+    snake.move()
 
 screen.exitonclick()
 
