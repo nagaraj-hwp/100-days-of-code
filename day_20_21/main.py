db1610329944b1528b7acffb2c25bdce197a23d9
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-02-21 22:54:41 +0530

    Day 25: Snake game esay hard level updated

--- day_20_21/main.py
+++ day_20_21/main.py
@@ -1,19 +1,38 @@
 # Snake game project
 from turtle import Screen
+import turtle as t
 from snake import Snake
 from food import Food
 from scoreboard import Scoreboard
 import time
 
-# Ideas for enhancement
-DIFFICULTY = 'e'
-WALL = 'y'
-screen_size = 'm'
+game_level = t.textinput("Choose Game Level", "easy or hard").lower()
+# print(game_level)
 
+if game_level == "easy":
+    time_interval = 0.25
+    screen_width = 800
+    screen_height = 800
+    screen_min_distance = -380
+    screen_max_distance = 380
+else:
+    time_interval = 0.1
+    screen_width = 600
+    screen_height = 600
+    screen_min_distance = -380
+    screen_max_distance = 380
+
+walls_enabled = t.textinput("Walls around (Your snake may lost without walls)", "Yes for walls or no").lower()
+# print(walls_enabled)
+
+if walls_enabled == "yes":
+    walls = True
+else:
+    walls = False
 
 screen = Screen()
 screen.bgcolor("black")
-screen.setup(width=600, height=600)
+screen.setup(width=screen_width, height=screen_height)
 screen.title("Epic Snake game")
 screen.tracer(0)
 
@@ -31,7 +50,7 @@ game_is_on = True
 
 while game_is_on:
     screen.update()
-    time.sleep(0.2)
+    time.sleep(time_interval)
     snake.move()
 
     # Detect collision with Food
@@ -41,9 +60,12 @@ while game_is_on:
         food.refresh()
 
     # Detect collision with wall
-    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+    if (snake.head.xcor() > screen_max_distance or snake.head.xcor() < screen_min_distance or snake.head.ycor() > screen_max_distance or snake.head.ycor() < screen_min_distance) and walls:
         game_is_on = False
         scoreboard.game_over()
+    elif not walls:
+        pass
+
 
     # Detect collision with the snakes tail
     for segment in snake.segments[1:]:
