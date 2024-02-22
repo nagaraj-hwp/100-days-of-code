396f1f3e6215dfe975e40852aae902d8f56d64b0
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-02-22 21:35:58 +0530

    Day 22: enhancement changes

--- day_22/main.py
+++ day_22/main.py
@@ -1,8 +1,3 @@
-# main execution file for pong game
-# step 1 : choose screen size
-# step 2 : create paddle
-# step 3: make paddles movable
-# step 4: make a ball
 import turtle
 from turtle import Screen
 from paddle import Paddle
