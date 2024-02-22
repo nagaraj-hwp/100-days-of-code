5241d18375a903e978c62f28bd3f1fdd0dc79595
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-02-22 21:32:42 +0530

    Day 22: paddle class file for completed pong game

--- day_22/paddle.py
+++ day_22/paddle.py
@@ -1,1 +1,25 @@
 # paddle control file for pong game
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+
+    def __init__(self, position, paddle_color="white", paddle_width=7):
+        super().__init__()
+        self.shape("square")
+        self.penup()
+        self.color(paddle_color)
+        self.shapesize(stretch_wid=paddle_width, stretch_len=1)
+        self.goto(position)
+
+    def go_up(self):
+        current_y = self.ycor()
+        if current_y < 250:
+            new_y = current_y + 20
+            self.setposition(self.xcor(), new_y)
+
+    def go_down(self):
+        current_y = self.ycor()
+        if current_y > -250:
+            new_y = current_y - 20
+            self.setposition(self.xcor(), new_y)
