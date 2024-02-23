7f0c2c6b6dfe8c188cf4cbe4d34632df240ecf02
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-02-23 21:51:36 +0530

    Day 23: capstone project with turtle module added main.py

---
+++ day_23/main.py
@@ -0,0 +1,22 @@
+# main.py
+import time
+from turtle import Screen, Turtle
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.tracer(0)
+
+tim = Player()
+
+screen.listen()
+screen.onkey(key="Up", fun=tim.move)
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
