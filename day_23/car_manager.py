06a717a030ef0bdf6718884b8d6ae807a900477d
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-02-23 21:53:31 +0530

    Day 23: capstone project with turtle module added scoreboard.py

---
+++ day_23/car_manager.py
@@ -0,0 +1,8 @@
+# car managing for the Turtle racing
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
+
+class CarManager:
+    pass
