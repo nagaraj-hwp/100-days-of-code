995c738c607ebe6959312d4434e574f023342aec
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-02-23 22:49:43 +0530

    Day 23: car manager modified

--- day_23/car_manager.py
+++ day_23/car_manager.py
@@ -1,8 +1,19 @@
 # car managing for the Turtle racing
+from turtle import Turtle
+import random
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
-class CarManager:
-    pass
+class CarManager(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.shape("square")
+        self.penup()
+        self.shapesize(stretch_len=1.5, stretch_wid=1.5)
+        self.color(random.choice(COLORS))
+
+    def move_car(self):
+        pass
