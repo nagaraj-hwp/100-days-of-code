b3d44fa8c5ce0f43f52695ffb4c3fd8f8cfe9eef
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-02-26 00:39:09 +0530

    Day 23: Turtle racing completed

--- day_23/scoreboard.py
+++ day_23/scoreboard.py
@@ -1,6 +1,28 @@
 # scoreboard for turtle racing game
-FONT = ("Courier", 60, "normal")
+from turtle import Turtle
+
+FONT = ("Courier", 16, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.hideturtle()
+        self.penup()
+        self.goto(-260, 260)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Level {self.level}", font=FONT, align="left")
+
+    def increase_level(self):
+        self.level += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"Game Over", font=FONT, align="center")
+
