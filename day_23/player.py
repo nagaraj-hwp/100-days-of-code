48e6efc85445777ef83a2cd34250578d3da19c5c
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-02-26 00:38:50 +0530

    Day 23: Turtle racing completed

--- day_23/player.py
+++ day_23/player.py
@@ -11,13 +11,23 @@ class Player(Turtle):
         super().__init__()
         self.shape("turtle")
         self.penup()
-        self.shapesize(stretch_len=1.5, stretch_wid=1.5)
+        self.shapesize(stretch_len=1, stretch_wid=1)
         self.color("brown3")
-        self.goto(STARTING_POSITION)
+        self.go_to_start()
         self.setheading(90)
+
+    def go_to_start(self):
+        self.goto(STARTING_POSITION)
 
     def move(self):
         current_y = self.ycor()
         if current_y < 280:
             new_y = current_y + 20
             self.setposition(self.xcor(), new_y)
+
+    def is_at_finish_line(self):
+        if self.ycor() > 260:
+            return True
+        else:
+            return False
+
