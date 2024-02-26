8d159e146a7a98cf882e5ea203db57bd39d5baef
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-02-26 19:56:09 +0530

    Day 24: Renamed files to be a txt files

--- day_24/mail-merge-project-start/main.py
+++ day_24/mail-merge-project-start/main.py
@@ -6,27 +6,36 @@
 # Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
 # Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
 # Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+########################################################################################
+# with open("./Input/Letters/starting_letter.txt") as letter:
+#     letter_content = letter.read()
+#
+# with open("./Input/Names/invited_names.txt") as names:
+#     name_list = names.readlines()
+#
+# for name in name_list:
+#     guest_name = name.strip()
+#     with open(f"./Output/ReadyToSend/letter_to_{guest_name}", "w") as guest_letter:
+#         for line in letter_content:
+#             if "[name]" in line:
+#                 line = line.replace("[name]", guest_name)
+#                 guest_letter.write(line)
+#             else:
+#                 guest_letter.write(line)
 
-with open("./Input/Letters/starting_letter.txt") as letter:
-    letter_content = letter.readlines()
-print(letter_content)
+###########################################################################3
 
-with open("./Input/Names/invited_names.txt") as names:
-    name_list = names.readlines()
-print(name_list)
+# Angela version
 
+PLACEHOLDER = "[name]"
 
-for line in letter_content:
-    if line != '\n':
-        print(line)
+with open("./Input/Names/invited_names.txt") as names_file:
+    name_list = names_file.readlines()
 
-for name in name_list:
-    guest_name = name.strip()
-    with open(f"./Output/ReadyToSend/letter_to_{guest_name}", "w") as guest_letter:
-        for line in letter_content:
-            if "[name]" in line:
-                line = line.replace("[name]", guest_name)
-                guest_letter.write(line)
-            else:
-                guest_letter.write(line)
-
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+    letter_content = letter_file.read()
+    for name in name_list:
+        stripped_name = name.strip()
+        new_letter = letter_content.replace(PLACEHOLDER, stripped_name)
+        with open(f"./Output/ReadyToSend/letter_to_{stripped_name}.txt", "w") as letter:
+            letter.write(new_letter)
