d350a299df86c2072659b0d919789bf9c28b7a5f
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-03-01 23:30:31 +0530

    Day 27: (day 32) Tkinter window label added

--- day_27/tkinter_basics.py
+++ day_27/tkinter_basics.py
@@ -6,5 +6,6 @@ window = tkinter.Tk()
 
 window.title("MY first UI with TK")
 window.minsize(width=500, height=600)
-
+label_1 = tkinter.Label(text="This is a sample label", font=("courier", 24))
+label_1.pack()
 window.mainloop()
