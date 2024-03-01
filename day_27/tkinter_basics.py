2df49214d6efd00e97ffb439a5fb7f6a5e18128e
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-03-01 23:39:30 +0530

    Day 27: (day 32) default arguments

--- day_27/tkinter_basics.py
+++ day_27/tkinter_basics.py
@@ -1,12 +1,22 @@
 #  Tkinter UI basics understanding
 
-import tkinter
+# import tkinter
+#
+# window = tkinter.Tk()
+#
+# window.title("MY first UI with TK")
+# window.minsize(width=500, height=600)
+# label_1 = tkinter.Label(text="This is a sample label", font=("courier", 24))
+# label_1.pack(side="left")
+#
+# window.mainloop()
 
-window = tkinter.Tk()
 
-window.title("MY first UI with TK")
-window.minsize(width=500, height=600)
-label_1 = tkinter.Label(text="This is a sample label", font=("courier", 24))
-label_1.pack(side="left")
+def fun(a=1, b=2, c=3):
+    print(a, b, c)
 
-window.mainloop()
+
+fun()  # 1 2 3
+fun(4, 5)  # 4 5 3
+fun(4, c=9)  # 4 2 9
+fun(b=10)  # 1 10 3
