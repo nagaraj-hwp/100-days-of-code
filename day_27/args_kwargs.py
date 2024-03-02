cd6c991f97b8f13232d1d4f66285bdf985bd1877
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-03-02 22:19:51 +0530

    Day 27: day33 args and kwargs understanding

---
+++ day_27/args_kwargs.py
@@ -0,0 +1,28 @@
+
+# def fun(a=1, b=2, c=3):
+#     print(a, b, c)
+#
+#
+# fun()  # 1 2 3
+# fun(4, 5)  # 4 5 3
+# fun(4, c=9)  # 4 2 9
+# fun(b=10)  # 1 10 3
+
+# variable arguments
+def add(*args):
+    total = 0
+    for n in args:
+        total += n
+    return total
+
+
+# print(add(1, 2, 3, 4, 5, 6))
+
+
+def calculate(n, **kwargs):
+    n += kwargs["add"]
+    n *= kwargs["multiply"]
+    print(n)
+
+
+calculate(2, add=10, multiply=5)
