0292371542681021616dfb874ac97b6863ea7f41
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-03-03 21:41:37 +0530

    Day 27: tkinter commit push

--- day_27/tkinter_basics.py
+++ day_27/tkinter_basics.py
@@ -1,6 +1,4 @@
 # Tkinter UI basics understanding
-
-
 import tkinter
 
 window = tkinter.Tk()
@@ -9,5 +7,7 @@ window.title("MY first UI with TK")
 window.minsize(width=500, height=600)
 label_1 = tkinter.Label(text="This is a sample label", font=("courier", 24))
 label_1.pack(side="left")
+button_1 = tkinter.Button(text="button A")
+button_1.pack()
 
 window.mainloop()
