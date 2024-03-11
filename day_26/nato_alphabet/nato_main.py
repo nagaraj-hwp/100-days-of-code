18a5837a49ebe5ddfd00dda3b05e7535d8bd9c9e
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-03-11 13:31:42 +0530

    Day 30: Nato phonetic project exceptions handling

--- day_26/nato_alphabet/nato_main.py
+++ day_26/nato_alphabet/nato_main.py
@@ -5,16 +5,19 @@ nato_phonetic = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_phonetic_dict = {row.letter: row.code for (index, row) in nato_phonetic.iterrows()}
 # print(nato_phonetic_dict)
 
-nato_created = False
-while not nato_created:
+
+def nato_create():
     name = input("Enter a name: ").upper()
     try:
         nato_phonetic_name = [nato_phonetic_dict[letter] for letter in name]
     except KeyError:
-        print("\nSorry, Only letters are allowed in the alphabet please.")
+        print("Sorry, Only letters are allowed in the alphabet please.")
+        nato_create()
     else:
-        print(f"\n{nato_phonetic_name}")
-        nato_created = True
+        print(f"{nato_phonetic_name}")
+
+
+nato_create()
 
 
 
