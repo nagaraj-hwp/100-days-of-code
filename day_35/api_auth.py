d00dc1eb8e2698a5fe1f15ec291822ffafd846cd
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-03-17 19:36:53 +0530

    Day 35: Twilio SMS send via cloud programming trigger enabled

--- day_35/api_auth.py
+++ day_35/api_auth.py
@@ -1,14 +1,17 @@
 # understanding API keys and authentication using python
-
-
 import requests
+from twilio.rest import Client
 
 with open("../ignore_dir/api_key.txt") as key_file:
-    api_key = key_file.read()
+    keys_data = key_file.readlines()
+    from_number = keys_data[5].strip("\n")
+    to_number = keys_data[7].strip("\n")
+    account_sid = keys_data[9].strip("\n")
+    auth_token = keys_data[11].strip("\n")
+    api_key = keys_data[0].strip("\n")
 
-
-MY_LAT = 17.05
-MY_LONG = 174.11
+MY_LAT = 29.11
+MY_LONG = 93.09
 
 base_url = "https://api.openweathermap.org/data/2.5/forecast"
 
@@ -16,14 +19,24 @@ weather_params = {
     "lat": MY_LAT,
     "lon": MY_LONG,
     "appid": api_key,
-    "cnt": 4
+    "cnt": 8
 }
 
 response = requests.get(url=base_url, params=weather_params)
 response.raise_for_status()
 weather_data = response.json()
 print(weather_data)
+will_rain = False
 for data in weather_data["list"]:
     if data["weather"][0]["id"] < 700:
         print("Bring an umbrella.")
-        break
+        will_rain = True
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        from_=from_number,
+        to=to_number,
+        body="Its going to rain today, bring an umbrella with you!."
+    )
+    print(message.sid)
+    print(message.status)
