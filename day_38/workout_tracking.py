ba32747325b8eeef10fbdb1b2be33cb66c94a018
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-03-20 22:36:04 +0530

    Day 38: Sheety API completed

--- day_38/workout_tracking.py
+++ day_38/workout_tracking.py
@@ -1,4 +1,5 @@
 import requests
+from datetime import datetime
 
 GENDER = "male"
 WEIGHT_KG = 72
@@ -31,3 +32,22 @@ input_json = {
 response = requests.post(url=exercise_endpoint, headers=headers, json=input_json)
 result = response.json()
 print(result)
+
+
+sheet_endpoint = "https://api.sheety.co/451dcd24930fcd95a679b9ea814007ce/myWorkouts/workouts"
+
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+
+for exercise in result["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today_date,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"]
+        }
+    }
+    sheet_response = requests.post(sheet_endpoint, json=sheet_inputs)
+    print(sheet_response.text)
