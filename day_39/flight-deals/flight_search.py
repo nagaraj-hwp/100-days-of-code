7152e1cc1d23c5b84dcb1f5c5e1daff3b9608c5b
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-04-06 23:27:55 +0530

    Day 39: Flight deals sheety update enabled

--- day_39/flight-deals/flight_search.py
+++ day_39/flight-deals/flight_search.py
@@ -1,3 +1,15 @@
+import requests
+
+TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
+
+with open("../../ignore_dir/api_key.txt") as key_file:
+    key_data = key_file.readlines()
+    TEQUILA_API_KEY = key_data[30].strip()
+
+
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
-    pass
+
+    def get_destination_code(self, city_name):
+        # Return "TESTING" for now to make sure Sheety is working. Get TEQUILA API data later.
+        code = "TESTING"
+        return code
