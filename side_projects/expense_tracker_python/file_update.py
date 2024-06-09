78e43659962cbe16678c2998a98a31581c3d503e
Nagaraj-hwp <nagarajpaul646@gmail.com>
2024-06-09 23:22:17 +0530

    Comment addded

--- side_projects/expense_tracker_python/file_update.py
+++ side_projects/expense_tracker_python/file_update.py
@@ -60,6 +60,7 @@ def get_expense_date(expense_day):
         today = dt.date.today()
         day_to_date = str(today - dt.timedelta(days=1))
     else:
+        # Enter date manually with caution
         # date_match = re.search('((\\d{4})-(\\d{2})-(\\d{2}))', expense_day)
         # expense_date = date_match[0]
         day_to_date = expense_day
