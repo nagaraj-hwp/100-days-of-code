48e9ab815a6dc3547c49192380c5e8d9719b1c4b
Nagaraj P <45648924+nagaraj-hwp@users.noreply.github.com>
2024-06-10 23:01:15 +0530

    Update main.py

--- side_projects/expense_tracker_python/main.py
+++ side_projects/expense_tracker_python/main.py
@@ -4,7 +4,7 @@ import file_update
 
 
 def initiate_expense_calculation():
-    expense_type = input("Enter expense type, whether a whole day expenses or single expense: ")
+    expense_type = input("Enter expense type, whether a complete day expenses or single expense: ")
     if expense_type == "d" or expense_type == "day":
         input_day = input("Enter date of the expense(YYYY−MM−DD) or just 'today' or 'td' or 'yesterday' or 'y'"
                           "(careful with your expense date input): ")
